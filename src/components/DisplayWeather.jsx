21411e1337841b6fceb05e26674860b7a88c1973
Anthony P <43166263+anthonypz@users.noreply.github.com>
2022-07-08 21:22:46 -0700

    Handle edge cases Fixes results for countries with no capital like Antarctica.

--- src/components/DisplayWeather.jsx
+++ src/components/DisplayWeather.jsx
@@ -5,7 +5,10 @@ const DisplayWeather = ({ filterCountries }) => {
   const [currentCountry, setCurrentCountry] = useState('');
 
   useEffect(() => {
-    if (filterCountries[0].name.common !== currentCountry) {
+    if (
+      filterCountries[0].capital &&
+      filterCountries[0].name.common !== currentCountry
+    ) {
       const [lat, lng] = filterCountries[0].capitalInfo.latlng;
       fetch(
         `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${
@@ -28,7 +31,7 @@ const DisplayWeather = ({ filterCountries }) => {
           <strong>Temperature:</strong> {weather.main.temp}℉ / {celsius}℃
         </p>
         <img
-          src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+          src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
           alt="weather icon"
         />
         <p>
