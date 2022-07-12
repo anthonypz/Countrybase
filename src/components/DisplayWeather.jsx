fb436d7f492005a019d33530c23760366b875878
Anthony P <43166263+anthonypz@users.noreply.github.com>
2022-07-11 18:53:36 -0700

    Style weather component

--- src/components/DisplayWeather.jsx
+++ src/components/DisplayWeather.jsx
@@ -1,27 +1,31 @@
+import { CardContent, Typography } from '@mui/material';
+
 const DisplayWeather = ({ weather }) => {
   const celsius = ((weather.main.temp - 32) / 1.8).toFixed(2);
   const kilometersPerHour = (weather.wind.speed * 1.609344).toFixed(2);
   return (
-    <>
-      <h2>Weather in {weather.name}</h2>
-      <p>
-        <strong>Temperature:</strong> {weather.main.temp}℉ / {celsius}℃
-      </p>
-      <p>
-        <strong>Humidity:</strong> {weather.main.humidity}%
-      </p>
-      <p>
-        <strong>Description:</strong> {weather.weather[0].description || 'N/A'}
-      </p>
+    <CardContent sx={{ paddingTop: 0 }}>
+      <Typography variant="h5" component="div" gutterBottom>
+        Weather in {weather.name}
+      </Typography>
       <img
         src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
         alt="weather icon"
       />
-      <p>
+      <Typography variant="body1" gutterBottom>
+        <strong>Description:</strong> {weather.weather[0].description || 'N/A'}
+      </Typography>
+      <Typography variant="body1" gutterBottom>
+        <strong>Temperature:</strong> {weather.main.temp}℉ / {celsius}℃
+      </Typography>
+      <Typography variant="body1" gutterBottom>
+        <strong>Humidity:</strong> {weather.main.humidity}%
+      </Typography>
+      <Typography variant="body1" gutterBottom>
         <strong>Wind:</strong> {weather.wind.speed} mph / {kilometersPerHour}{' '}
         kph
-      </p>
-    </>
+      </Typography>
+    </CardContent>
   );
 };
 
